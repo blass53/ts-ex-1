41280d97fe448a84ec4e2d772c6c7ea6a8e06e1e
Blai Fernández Bravo <94699337+blass53@users.noreply.github.com>
2022-05-09 11:34:51 +0200

    Add files via upload

---
+++ resistor-color.ts
@@ -0,0 +1,7 @@
+
+export const colorCode = (col: string) => {
+
+  return COLORS.indexOf(col);
+}
+
+export const COLORS: Array<string> = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
